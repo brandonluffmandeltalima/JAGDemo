2e1db4fe17c6047944360e5333759c87777c76c6
Brandon Luffman <brandonl@deltalima.ai>
2025-11-28 20:57:05 -0500

    new

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import "@/styles/globals.css";
+import '../styles/globals.css';
+import '../styles/navbar.css';
+import '../styles/demo.css';
+import '../styles/mvp.css';
+import '../styles/admin.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
